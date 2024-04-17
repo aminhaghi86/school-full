a8a1e5c7b797d93e224211cf76ae0f7820503ac6
amin86yehego <amin@yehego.com>
2024-04-17 11:04:14 +0200

    remove commentout code

--- server/controller/scheduleController.js
+++ server/controller/scheduleController.js
@@ -30,39 +30,12 @@ const getScheduleById = async (req, res) => {
   }
 };
 
-// const createSchedule = async (req, res) => {
-//   try {
-//     const userId = req.user.id;
-//     const { start, end, title, description, course } = req.body;
-//     const schedule = await Schedule.create({
-//       start,
-//       end,
-//       title,
-//       description,
-//       course,
-//       userId,
-//     });
-//     const io = getIO();
-//     const teacherSocketId = getTeacherSocketId(userId);
-//     console.log('teacherSocketId',teacherSocketId);
-//     if (teacherSocketId) {
-//       io.to(teacherSocketId).emit("scheduleCreated", schedule);
-//     }
-//     // const socketId = getTeacherSocketId(userId);
-//     // io.emit("scheduleCreated", schedule);
-//     res.status(201).json(schedule);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// };
-
 // Update a schedule
 const createSchedule = async (req, res) => {
   try {
     const userId = req.user.id;
     const { start, end, title, description, course } = req.body;
-    console.log('Received user ID:', userId); // Log user ID
+    console.log("Received user ID:", userId); // Log user ID
     const schedule = await Schedule.create({
       start,
       end,
@@ -73,11 +46,11 @@ const createSchedule = async (req, res) => {
     });
     const io = getIO();
     const teacherSocketId = getTeacherSocketId(userId);
-    console.log('Retrieved teacher socket ID:', teacherSocketId); // Log teacher socket ID
+    console.log("Retrieved teacher socket ID:", teacherSocketId); // Log teacher socket ID
     if (teacherSocketId) {
       io.to(teacherSocketId).emit("scheduleCreated", schedule);
     } else {
-      console.log('No teacher socket ID found for user ID:', userId); // Log if teacher socket ID is not found
+      console.log("No teacher socket ID found for user ID:", userId); // Log if teacher socket ID is not found
     }
     res.status(201).json(schedule);
   } catch (error) {
